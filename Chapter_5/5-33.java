d9b01247a5fa511db4244e33a9b3e21f67407ee4
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:43:23 -0600

    Update 5-33.java

--- Chapter_5/5-33.java
+++ Chapter_5/5-33.java
@@ -2,6 +2,13 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 5-33
 Date: .  2018
-Summary:
+Summary: (Perfect number) A positive integer is called 
+          a perfect number if it is equal to the sum of 
+          all of its positive divisors, excluding itself. 
+          For example, 6 is the first perfect number because 
+          6 = 3 + 2 + 1. The next is 28 = 14 + 7 + 4 + 2 + 1. 
+          There are four perfect numbers less than 10,000. 
+          Write a program to find all these four numbers.
 */
