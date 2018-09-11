70cd28667bd84f47699e3b693a0bcc3bb1335d8e
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:38:42 -0600

    Update 4-1.java

--- Chapter_4/4-1.java
+++ Chapter_4/4-1.java
@@ -2,6 +2,22 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 4-1
 Date: .  2018
-Summary:
+Summary: (Geometry: area of a pentagon) Write a program that 
+          prompts the user to enter the length from the center 
+          of a pentagon to a vertex and computes the area of 
+          the pentagon, as shown in the following figure.
+          
+          The formula for computing the area of a pentagon is:
+          
+              Area = (5 * s^2) / [4 * tan(pi/5)]
+            
+           where s is the length of a side. The side can be 
+           computed using the formula:
+           
+              s = 2r sin 5
+              
+            where r is the length from the center of a pentagon 
+            to a vertex. Round up two digits after the decimal point.
 */
