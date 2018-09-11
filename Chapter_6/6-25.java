aae141a135d53d5c302149250c641decdce892af
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:48:12 -0600

    Update 6-25.java

--- Chapter_6/6-25.java
+++ Chapter_6/6-25.java
@@ -2,6 +2,16 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 6-25
 Date: .  2018
-Summary:
+Summary: (Convert milliseconds to hours, minutes, and seconds) 
+          Write a method that converts milliseconds to hours, 
+          minutes, and seconds using the following header:
+          
+              public static String convertMillis(long millis)
+              
+          The method returns a string as hours:minutes:seconds. 
+          For example, convertMillis(5500) returns a string 0:0:5, 
+          convertMillis(100000) returns a string 0:1:40, and 
+          convertMillis(555550000) returns a string 154:19:10.
 */
