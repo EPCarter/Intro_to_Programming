7c610daaa3dd876c0b13188d20fc617dfe0b6959
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:41:54 -0600

    Update 5-11.java

--- Chapter_5/5-11.java
+++ Chapter_5/5-11.java
@@ -2,6 +2,11 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 5-11
 Date: .  2018
-Summary:
+Summary: (Find numbers divisible by 5 or 6, but not both) 
+          Write a program that displays all the numbers 
+          from 100 to 200, ten per line, that are divisible 
+          by 5 or 6, but not both. Numbers are separated by 
+          exactly one space.
 */
