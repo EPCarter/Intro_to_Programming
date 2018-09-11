9710cd40622676aae9db45479b840068a9e78535
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 17:31:54 -0600

    Update 2-23.java

--- Chapter_2/2-23.java
+++ Chapter_2/2-23.java
@@ -8,3 +8,9 @@ Summary: (Cost of driving) Write a program that prompts the user to enter
           the distance to drive, the fuel efficiency of the car in miles 
           per gallon, and the price per gallon, and displays the cost of the trip.
 */
+
+/*
+Main{
+
+}
+*/
