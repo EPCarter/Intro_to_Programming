95d66c7b635041cbc76076b08d29ea21298b47c5
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 15:43:17 -0600

    Create 1-5.java

---
+++ 1-5.java
@@ -0,0 +1,6 @@
+/*
+Author: Erik Carter
+Email: Carter.Eri7200@stu.stech.edu
+Date: Sept. 11 2018
+Summary: Compute expressions) Write a program that displays the result of: [(9.5 * 4.5) - (2.5 * 3)] / (45.5 - 3.5) (PEMDAS)
+*/
