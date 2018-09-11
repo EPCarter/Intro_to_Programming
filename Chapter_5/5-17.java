227dda6af73c1e94d07300058904e037f8a16369
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:42:33 -0600

    Update 5-17.java

--- Chapter_5/5-17.java
+++ Chapter_5/5-17.java
@@ -2,6 +2,10 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 5-17
 Date: .  2018
-Summary:
+Summary: (Display pyramid) Write a program that prompts
+          the user to enter an integer from 1 to 15 and 
+          displays a pyramid, as shown in the following 
+          sample run.
 */
