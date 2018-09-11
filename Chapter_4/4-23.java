8dea3abeea507450fc2410ebf7b9cba697d117e1
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:40:57 -0600

    Update 4-23.java

--- Chapter_4/4-23.java
+++ Chapter_4/4-23.java
@@ -2,6 +2,19 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 4-23
 Date: .  2018
-Summary:
+Summary: (Financial application: payroll) Write a program 
+          that reads the following information and prints 
+          a payroll statement:
+
+             Employee’s name (e.g., Smith)
+             Number of hours worked in a week (e.g., 10)
+             Hourly pay rate (e.g., 9.75)
+             Federal tax withholding rate (e.g., 20%)
+             State tax withholding rate (e.g., 9%)
+
+        Remember to not use nextLine() after nextInt() for reasons 
+        explained on page 480. This will case a logic error when 
+        running the program.
 */
