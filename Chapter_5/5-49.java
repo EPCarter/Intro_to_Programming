bac96e34eab59c8d07a59335e42162a2eb80821d
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:44:12 -0600

    Update 5-49.java

--- Chapter_5/5-49.java
+++ Chapter_5/5-49.java
@@ -2,6 +2,15 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 5-49
 Date: .  2018
-Summary:
+Summary: (Count vowels and consonants) Assume letters 
+          A, E, I, O, and U as the vowels. Write a 
+          program that prompts the user to enter a 
+          string and displays the number of vowels 
+          and consonants in the string.
+
+          Hint: Use the method "toUpperCase()" on your 
+          character to simplify the conditions of your 
+          if statement
 */
