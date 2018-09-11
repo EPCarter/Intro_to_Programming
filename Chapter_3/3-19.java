1cc5d8e70dcaa09126ca9ec78537279a1650821d
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:34:59 -0600

    Update 3-19.java

--- Chapter_3/3-19.java
+++ Chapter_3/3-19.java
@@ -2,6 +2,11 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 3-19
 Date: .  2018
-Summary:
+Summary: (Compute the perimeter of a triangle) Write a program 
+          that reads three edges for a triangle and computes the 
+          perimeter if the input is valid. Otherwise, display that 
+          the input is invalid. The input is valid if the sum of 
+          every pair of two edges is greater than the remaining edge.
 */
