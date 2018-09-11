82cfb32eafd2965957fdc5840da313a0a109e5ba
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 17:02:50 -0600

    Update Final_Project.java

--- Final_Project/Final_Project.java
+++ Final_Project/Final_Project.java
@@ -1,1 +1,21 @@
+/*
+Author: Erik Carter
+Email: Carter.Eri7200@stu.stech.edu
+Course: CSCI 1105 - Introduction to Programming
+Assignment: 1-13
+Date: .  2018
+Summary: Think of a project that uses multiple items you have learned 
+         from this class (using arrays is required). Please create a 
+         write-up proposal for your project following the format stated below.
 
+              1. Introduction: Overview of your project (1 paragraph)
+              2. Target Population: Who are you directing this software and 
+                  what are you doing to make it appeal to them? (2 paragraphs)
+              3. Purpose to the consumer: what makes your program unique, 
+                  what makes it a better option than other software or is 
+                  there a lack of software in this field? Is it helpful, how so? (3 paragraphs)
+              4. Constraints: List possible bugs and what factors you will 
+                  be considering beforehand to avoid those bugs. (Numbered list and 2 paragraphs)
+              5. Conclusion: Why you are moving forward with this project. 
+                  Overview of project strategy. (1 - 2 paragraphs)
+ */
