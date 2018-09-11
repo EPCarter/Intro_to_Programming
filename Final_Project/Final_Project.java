c2ccf182cb518e7313bc9dbaae55b2dc44eb859a
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 17:04:43 -0600

    Update Final_Project.java

--- Final_Project/Final_Project.java
+++ Final_Project/Final_Project.java
@@ -13,9 +13,25 @@ Summary: Think of a project that uses multiple items you have learned
                   what are you doing to make it appeal to them? (2 paragraphs)
               3. Purpose to the consumer: what makes your program unique, 
                   what makes it a better option than other software or is 
-                  there a lack of software in this field? Is it helpful, how so? (3 paragraphs)
+                  there a lack of software in this field? Is it helpful, how so? 
+                  (3 paragraphs)
               4. Constraints: List possible bugs and what factors you will 
-                  be considering beforehand to avoid those bugs. (Numbered list and 2 paragraphs)
+                  be considering beforehand to avoid those bugs. (Numbered list 
+                  and 2 paragraphs)
               5. Conclusion: Why you are moving forward with this project. 
                   Overview of project strategy. (1 - 2 paragraphs)
+                  
+           GitHub is a great way to market your product and show off your code.  
+           The formatting and information of your README page is especially helpful 
+           to give your target audience the best impression of your code.  Here is
+           a template (Links to an external site.)Links to an external site. for a 
+           GitHub README page along with general guidelines (Links to an external 
+           site.)Links to an external site. to follow and reasons (Links to an external 
+           site.)Links to an external site.why someone wouldn't use your code.  Read 
+           through these and use Mr. Perkins's Angular.io repository to help shape 
+           your own README page.
+           
+           Please add all files needed to run your code, a JavaDoc for your code, 
+           and a README for your project using the examples above onto your GitHub 
+           and submit the link here.
 */
