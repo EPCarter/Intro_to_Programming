00ee0bedcea83c0e8a3db5aff87874687f0a25c7
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:19:18 -0600

    Update 1-9.java

--- Chapter_1/1-9.java
+++ Chapter_1/1-9.java
@@ -2,6 +2,9 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 1-9
 Date: .  2018
-Summary:
+Summary: (Area and perimeter of a rectangle) Write a program that displays 
+          the area and perimeter of a rectangle with the width of 4.5 and height 
+          of 7.9 using the following formula: area = width * height
 */
