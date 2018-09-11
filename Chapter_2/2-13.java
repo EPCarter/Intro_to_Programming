0d9438802070a8ed87d82293298f61173720abfd
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:30:05 -0600

    Update 2-13.java

--- Chapter_2/2-13.java
+++ Chapter_2/2-13.java
@@ -2,6 +2,25 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 2-13
 Date: .  2018
-Summary:
+Summary: (Financial application: compound value) Suppose you save $100 each month 
+          into a savings account with the annual interest rate 5%. Thus, the monthly 
+          interest rate is 0.05/12 = 0.00417. After the first month, the value in the account becomes
+
+                100 * (1 + 0.00417) = 100.417
+
+            After the second month, the value in the account becomes
+
+                (100 + 100.417) * (1 + 0.00417) = 201.252
+
+            After the third month, the value in the account becomes
+
+                (100 + 201.252) * (1 + 0.00417) = 302.507
+
+             and so on.
+
+          Write a program that prompts the user to enter a monthly saving amount and 
+          displays the account value after the sixth month. (In Exercise 5.30, you will 
+          use a loop to simplify the code and display the account value for any month.)
 */
