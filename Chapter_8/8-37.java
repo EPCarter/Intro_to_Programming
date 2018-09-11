431524d5e54002804b95c74d6e8520b1642c2452
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 17:00:18 -0600

    Update 8-37.java

--- Chapter_8/8-37.java
+++ Chapter_8/8-37.java
@@ -2,6 +2,17 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 8-37
 Date: .  2018
-Summary:
+Summary: (Guess the capitals) Write a program that repeatedly 
+          prompts the user to enter a capital for a state. Upon
+          receiving the user input, the program reports whether 
+          the answer is correct. Assume that 10 states and their 
+          capitals are stored in a two-dimensional array, as shown 
+          in Figure 8.10. The program prompts the user to answer 
+          states’ capitals and displays the total correct count. 
+          The user’s answer is not case-sensitive.
+
+          This assignment has been edited from the book version to 
+          use an array that stores only 10 states and capitals.
 */
