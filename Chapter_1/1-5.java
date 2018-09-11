00c2a603ac7f9a0e16b67e586112817a30749ddd
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:12:25 -0600

    Update 1-5.java

--- Chapter_1/1-5.java
+++ Chapter_1/1-5.java
@@ -1,6 +1,7 @@
 /*
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
-Date: Sept. 11 2018
-Summary: Compute expressions) Write a program that displays the result of: [(9.5 * 4.5) - (2.5 * 3)] / (45.5 - 3.5) (PEMDAS)
+Course: CSCI 1105 - Introduction to Programming
+Date: .  2018
+Summary:
 */
