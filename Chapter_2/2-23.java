d78581fe539094ace125e5cc826ce8e9902aed85
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:31:20 -0600

    Update 2-23.java

--- Chapter_2/2-23.java
+++ Chapter_2/2-23.java
@@ -2,6 +2,9 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 2-23
 Date: .  2018
-Summary:
+Summary: (Cost of driving) Write a program that prompts the user to enter 
+          the distance to drive, the fuel efficiency of the car in miles 
+          per gallon, and the price per gallon, and displays the cost of the trip.
 */
