ea75f6a1c84df4d5ba76f2e16e0b87572ab8842b
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:47:01 -0600

    Update 6-17.java

--- Chapter_6/6-17.java
+++ Chapter_6/6-17.java
@@ -2,6 +2,14 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 6-17
 Date: .  2018
-Summary:
+Summary: (Display matrix of 0s and 1s) Write a method that displays an 
+          n-by-n matrix using the following header:
+          
+              public static void printMatrix(int n)
+              
+          Each element is 0 or 1, which is generated randomly. Write a 
+          test program that prompts the user to enter n and displays an 
+          n-by-n matrix.
 */
