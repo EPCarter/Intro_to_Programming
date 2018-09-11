d97578f455b92800a15656abd69ca7637f13783e
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:39:22 -0600

    Update 4-17.java

--- Chapter_4/4-17.java
+++ Chapter_4/4-17.java
@@ -2,6 +2,10 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 4-17
 Date: .  2018
-Summary:
+Summary: (Days of a month) Write a program that prompts the user 
+          to enter a year and the first three letters of a month 
+          name (with the first letter in uppercase) and displays 
+          the number of days in the month.
 */
