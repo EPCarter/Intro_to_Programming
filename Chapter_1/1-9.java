f25bacb730ab3e7e3015568241fb9ff16d6523a2
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 17:21:05 -0600

    Update 1-9.java

--- Chapter_1/1-9.java
+++ Chapter_1/1-9.java
@@ -8,3 +8,18 @@ Summary: (Area and perimeter of a rectangle) Write a program that displays
           the area and perimeter of a rectangle with the width of 4.5 and height 
           of 7.9 using the following formula: area = width * height
 */
+
+/*
+Main{
+          int Area;
+          int Perimeter;
+          int Width = 4.5;
+          int Height = 7.9;
+          
+          Area = Width * Height
+          Perimeter = (Width + Height) * 2
+          
+          println "Area:" Area;
+          println "Perimeter:" Perimeter;
+}
+*/
