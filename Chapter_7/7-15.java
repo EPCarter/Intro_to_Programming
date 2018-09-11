76e7719dfd7097c101049c60a5b0310f55231d26
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:53:23 -0600

    Update 7-15.java

--- Chapter_7/7-15.java
+++ Chapter_7/7-15.java
@@ -2,6 +2,20 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 7-15
 Date: .  2018
-Summary:
+Summary: (Eliminate duplicates) Write a method that returns a 
+          new array by eliminating the duplicate values in the 
+          array using the following method header:
+          
+              public static int[] eliminateDuplicates(int[] list)
+              
+          Write a test program that reads in ten integers, invokes 
+          the method, and displays the result.
+
+          (Hint: Use a for loop to read in the 10 values from the user. 
+          Create a method for eliminating the duplicates which accepts 
+          the user's array as a parameter. In the method, create a new 
+          array for storing the non-duplicates with the same size as 
+          the user's array.)
 */
