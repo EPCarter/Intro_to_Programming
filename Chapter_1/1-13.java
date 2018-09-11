567d69fd26da01e8ec6c44cfb0d333b764863a68
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:23:33 -0600

    Update 1-13.java

--- Chapter_1/1-13.java
+++ Chapter_1/1-13.java
@@ -2,6 +2,18 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 1-13
 Date: .  2018
-Summary:
+Summary: (Algebra: solve 2 * 2 linear equations) You can use Cramer’s rule 
+          to solve the following 2 * 2 system of linear equation:
+            ax + by = e
+            x = (ed - bf) / (ad - bc)
+            cx + dy - f
+            y = (af - ec) / (ad - bc)
+          
+          Write a program that solves the following equation and displays the value for x and y:
+            3.4x + 50.2y = 44.5
+            2.1x + 0.55y = 5.9
+
+          (Hint: Use the equations given first to identify what numeric value replaces which number. In this case, a is 3.4.)
 */
