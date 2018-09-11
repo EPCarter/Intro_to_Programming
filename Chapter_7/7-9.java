be3928a5e75daa0aa27b72a91ca0eb8a3e84df7a
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:52:15 -0600

    Update 7-9.java

--- Chapter_7/7-9.java
+++ Chapter_7/7-9.java
@@ -2,6 +2,15 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 7-9
 Date: .  2018
-Summary:
+Summary: (Find the smallest element) Write a method that finds
+          the smallest element in an array of double values 
+          using the following header:
+          
+              public static double min(double[] array)
+              
+          Write a test program that prompts the user to enter 
+          ten numbers, invokes this method to return the minimum 
+          value, and displays the minimum value.
 */
