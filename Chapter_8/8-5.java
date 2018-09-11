352c0b77cda20785bd2700f5d8353297deea7377
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:57:23 -0600

    Update 8-5.java

--- Chapter_8/8-5.java
+++ Chapter_8/8-5.java
@@ -2,6 +2,20 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 8-5
 Date: .  2018
-Summary:
+Summary: (Algebra: add two matrices) Write a method to add two matrices. 
+          The header of the method is as follows:
+          
+              public static double[][] addMatrix(double[][] a, double[][] b)
+          
+          In order to be added, the two matrices must have the same dimensions
+          and the same or compatible types of elements. Let c be the resulting 
+          matrix. Each element c[i][j] is a[i][j]+b[i][j].
+          
+              Example:
+
+                  1.0   2.0   3.0   0.0  2.0  4.0   1.0  4.0  7.0
+                  4.0   5.0   6.0 + 1.0  4.5  2.2 = 5.0  9.5  8.2
+                  7.0   8.0   9.0   1.1  4.3  5.2   8.1  12.3  14.2
 */
