06776a510aa1fe6e8df281408e76d806d1226beb
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:45:45 -0600

    Update 6-3.java

--- Chapter_6/6-3.java
+++ Chapter_6/6-3.java
@@ -2,6 +2,18 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 6-3
 Date: .  2018
-Summary:
+Summary: (Palindrome integer) Write the methods with the following headers:
+            
+              // Return the reversal of an integer, i.e., reverse(456) returns 654
+              public static int reverse(int number) 
+              // Return true if number is a palindrome
+              public static boolean isPalindrome(int number)
+
+          Use the reverse method to implement isPalindrome. A number is a palindrome 
+          if its reversal is the same as itself. Write a test program that prompts the 
+          user to enter an integer and reports whether the integer is a palindrome.
+
+          (Hint: The isPalindrome method should call the reverse method.)
 */
