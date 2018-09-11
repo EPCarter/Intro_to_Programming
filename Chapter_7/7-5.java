de735aeeb4be8da42980a082fdef3eb7efef4882
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-11 16:51:06 -0600

    Update 7-5.java

--- Chapter_7/7-5.java
+++ Chapter_7/7-5.java
@@ -2,6 +2,15 @@
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
+Assignment: 7-5
 Date: .  2018
-Summary:
+Summary: (Print distinct numbers) Write a program that reads in ten 
+          numbers and displays the number of distinct numbers and 
+          the distinct numbers separated by exactly one space 
+          (i.e., if a number appears multiple times, it is displayed only once).
+
+          (Hint: Read a number and store it to an array if it is new. 
+          If the number is already in the array, ignore it.)
+
+          After the input, the array contains the distinct numbers.
 */
