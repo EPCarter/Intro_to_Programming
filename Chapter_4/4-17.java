0b1674b927bba1763961d725268380d33f1425d9
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-12 15:44:25 -0600

    Update 4-17.java

--- Chapter_4/4-17.java
+++ Chapter_4/4-17.java
@@ -13,5 +13,56 @@ Summary: (Days of a month) Write a program that prompts the user
 /*
 Main{
 
+          int year = userinput "Enter Year (YYYY): "
+          string month = userinput "Enter Month (Mmm): "
+          string hads = has;
+          int days = 31;
+          
+          //TODO format month correctly (Take three letters, 1st uppercase, 2-3 lowercase)
+          
+          if(year < 2018){
+                    string hads = "had";
+           }
+           else{
+                    string hads = "has";
+           }
+           
+           if(year = leap && month = "Feb"){
+                    days = 29;
+           }
+           elseif(month=[]){
+                    days = 30;
+           }
+           else{
+                    days = 31;
+           }
+           Leap Year 
+           
+                if(year > 1582){
+                //Gregorian 1584AD + 4
+                       28 [Feb]
+                       29 [Feb]
+                       30 [Apr, Jun, Sep, Nov]
+                       31 [Jan, Mar, May, Jul, Aug, Oct, Dec]
+                    }
+                    
+              elseif(year >= -45 && year <= 1582){
+                    //Julian Jan 45BCE - Oct 1582AD
+                       28 [Feb]
+                       29 [Feb]
+                       30 [Apr, Jun, Sep, Nov]
+                       31 [Jan, Mar, May, Jul, Aug, Oct, Dec]
+                    }
+                    
+              elseif(year < -45){
+                     //Roman Republic
+                       23 [Wag] //Month of wages (Mercedonius Mensis Intercalari)
+                       28 [Feb]
+                       29 [Jan, Apr, Jun, Aug, Sep, Nov, Dec]
+                       31 [Mar, May, Jul, Oct]
+                       }
+
+          
+          System.out.println("The month of " + month + "in " + year + hads + days + "days in it.");
 }
 */
