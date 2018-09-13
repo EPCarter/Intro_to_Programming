9bc8c69139715981802004745252e642240da063
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-12 19:32:12 -0600

    Update 4-17.java

--- Chapter_4/4-17.java
+++ Chapter_4/4-17.java
@@ -1,70 +1,74 @@
-/*
-Author: Erik Carter
-Email: Carter.Eri7200@stu.stech.edu
-Course: CSCI 1105 - Introduction to Programming
-Assignment: 4-17
-Date: .  2018
-Summary: (Days of a month) Write a program that prompts the user 
-          to enter a year and the first three letters of a month 
-          name (with the first letter in uppercase) and displays 
-          the number of days in the month.
-*/
+	/*
+	Author: Erik Carter
+	Email: Carter.Eri7200@stu.stech.edu
+	Course: CSCI 1105 - Introduction to Programming
+	Assignment: 4-17
+	Date: .  2018
+	Summary: (Days of a month) Write a program that prompts the user to enter a year and the first three letters of a month  name (with the first letter in uppercase) and displays the number of days in the month.
+	*/
+	
+	import java.util.regex;
+	import java.util.Scanner;
 
-import java.util.regex
+	class fourSeventeen {
+		public static void main(String[] args) {
+			
+			Scanner reader = new Scanner(System.in);
+			int year = userinput "Enter Year (YYYY): "
+			string month = userinput "Enter Month (Mmm): "
+			string hads = has;
+			int days = 0;
+						
+//TODO format month correctly (Take three letters, 1st uppercase, 2-3 lowercase)
+						
+			if(year < 2018){
+				string hads = "had";
+				}
+			
+			else{
+				string hads = "has";
+				}
+						 
+			if(year = leap && month = "Feb"){
+				days = 29;
+				}
+			
+			else if(month=[]){
+				days = 30;
+						 }
+			 else{
+				days = 31;
+						 }
+						
+		 Leap Year 
+						 
+			if(year > 1582){
+			//Gregorian 1584AD + 4
+				28 [Feb]
+				29 [Feb]
+				30 [Apr, Jun, Sep, Nov]
+				31 [Jan, Mar, May, Jul, Aug, Oct, Dec]
+				}
+											
+			else if(year >= -45 && year <= 1582){
+			//Julian Jan 45BCE - Oct 1582AD
+				28 [Feb]
+				29 [Feb]
+				30 [Apr, Jun, Sep, Nov]
+				31 [Jan, Mar, May, Jul, Aug, Oct, Dec]
+											}
+											
+			else if(year < -45){
+			//Roman Republic
+				23 [Wag] //Month of wages (Mercedonius Mensis Intercalari)
+				28 [Feb]
+				29 [Jan, Apr, Jun, Aug, Sep, Nov, Dec]
+				31 [Mar, May, Jul, Oct]
+				}
 
-class fourSeventeen {
-	public static void main(String[] args) {
-
-          int year = userinput "Enter Year (YYYY): "
-          string month = userinput "Enter Month (Mmm): "
-          string hads = has;
-          int days = 0;
-          
-          //TODO format month correctly (Take three letters, 1st uppercase, 2-3 lowercase)
-          
-          if(year < 2018){
-                    string hads = "had";
-           }
-           else{
-                    string hads = "has";
-           }
-           
-           if(year = leap && month = "Feb"){
-                    days = 29;
-           }
-           elseif(month=[]){
-                    days = 30;
-           }
-           else{
-                    days = 31;
-           }
-           Leap Year 
-           
-                if(year > 1582){
-                //Gregorian 1584AD + 4
-                       28 [Feb]
-                       29 [Feb]
-                       30 [Apr, Jun, Sep, Nov]
-                       31 [Jan, Mar, May, Jul, Aug, Oct, Dec]
-                    }
-                    
-              else if(year >= -45 && year <= 1582){
-                    //Julian Jan 45BCE - Oct 1582AD
-                       28 [Feb]
-                       29 [Feb]
-                       30 [Apr, Jun, Sep, Nov]
-                       31 [Jan, Mar, May, Jul, Aug, Oct, Dec]
-                    }
-                    
-              else if(year < -45){
-                     //Roman Republic
-                       23 [Wag] //Month of wages (Mercedonius Mensis Intercalari)
-                       28 [Feb]
-                       29 [Jan, Apr, Jun, Aug, Sep, Nov, Dec]
-                       31 [Mar, May, Jul, Oct]
-                       }
-
-          
-          System.out.println("The month of " + month + "in " + year + hads + days + "days in it.");
-          }
+						
+		System.out.println("The month of " + month + "in " + year + hads + days + "days in it.");
+			}
+		}	
+	}
 }
