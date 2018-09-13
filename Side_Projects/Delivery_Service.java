cd85ad5078bd077a7c22be4fe16f7cf1b74b057b
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-13 16:31:18 -0600

    Create Delivery_Service.java

---
+++ Side_Projects/Delivery_Service.java
@@ -0,0 +1,76 @@
+
+/*
+Author: Erik Carter
+Email: Carter.Eri7200@stu.stech.edu
+Course: CSCI 1105 - Introduction to Programming
+Assignment:
+Date: Sept. 13, 2018
+
+Summary: Framework for a Delivery Service that acts as an additional step between sellers and buyers, acting as a quality assurance and assembly, removing packaging before sending on to the customer already ready to go, or as close as can, though with notes. 
+
+
+*/
+
+import java.util.Scanner;
+
+public class Delivery_Service{	
+  public static void main(String[] args) {		
+		Scanner reader = new Scanner(System.in);
+    
+    zero = "Lobby";
+    one = "Introductions";
+		two = "Customer";
+    three = "Headquarters";
+    four = "Hubs";
+    five = "3rd Party";
+    
+   System.out.println("Directory" + ln + ln + "0: " + zero + ln + "1st Floor: " + one + ln "2nd Floor: " + two + ln + "3rd Floor: " + three + ln + "4th Floor: " + four + ln + "5th Floor: " + five + ln);
+   System.out.print("Which Floor: ");
+    
+    try{
+    int floor = reader.nextInt();
+    
+    }
+    
+    catch(mismatch.Exception Exc){
+          //convert to string to number (ex. Nine = Floor 9 = 9), try again
+    }
+    
+    if(floor = -966){	
+    System.out.print("We all float down here..." + "Wuah Ha! " * downHere);
+   
+    }
+    
+    else{
+    floor = 
+        0 = Lobby: Help and Directory
+        1 = 1st Floor:
+        2 = 2nd Floor:
+        3 = 3rd Floor:
+     //go to floor number
+     }
+     
+   
+   one{
+      0: Directory change floor name or Lobby option
+   
+   }
+   
+   two{
+   }
+   
+   three{
+   }
+   
+   four{
+   }
+   
+   five{
+   }
+   
+   
+   
+   
+   }
+  }
+}
