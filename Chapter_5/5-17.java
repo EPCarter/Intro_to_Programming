3f5fcdfd489bdb6eef14ce14401e82d8228f202c
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-12 19:50:27 -0600

    Update 5-17.java

--- Chapter_5/5-17.java
+++ Chapter_5/5-17.java
@@ -8,11 +8,26 @@ Summary: (Display pyramid) Write a program that prompts
           the user to enter an integer from 1 to 15 and 
           displays a pyramid, as shown in the following 
           sample run.
+          
+          Enter Number of Lines: 3 (user input)
+          
+           1
+          212
+         32123
 */
 import java.util.Scanner;
 
 class fiveSeventeen {
-          public static void main(String[] args) {
-           Scanner reader = new Scanner(System.in);         
+         public static void main(String[] args) {
+                    Scanner reader = new Scanner(System.in);  
+                    int lines = reader.nextInt();
+                   
+                   Array1[]
+                   Array2[]
+                   Array3[]
+                       ...15
+                             
+                  System.out.println("Lines Given: " + lines);
+                  System.out.println(array);
           }
 }
