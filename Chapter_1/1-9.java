cdcf97a9217ff78d10cb098196692c739dba38a4
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-17 15:14:25 -0600

    Update 1-9.java

--- Chapter_1/1-9.java
+++ Chapter_1/1-9.java
@@ -1,25 +1,22 @@
 /*
-Author: Erik Carter
-Email: Carter.Eri7200@stu.stech.edu
-Course: CSCI 1105 - Introduction to Programming
-Assignment: 1-9
-Date: .  2018
-Summary: (Area and perimeter of a rectangle) Write a program that displays 
-          the area and perimeter of a rectangle with the width of 4.5 and height 
-          of 7.9 using the following formula: area = width * height
+	Author: Erik Carter
+	Email: Carter.Eri7200@stu.stech.edu
+	Course: CSCI 1105 - Introduction to Programming
+	Assignment: 1-9
+	Date: Sept. 17, 2018
+	Summary: (Area and perimeter of a rectangle) Write a program that displays the area and perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula: area = width * height
 */
 
-/*
-Main{
-     float Area;
-     float Perimeter;
-     float Width = 4.5;
-     float Height = 7.9;
-          
-      Area = Width * Height
-      Perimeter = (Width + Height) * 2
-          
-      println "Area:" Area;
-      println "Perimeter:" Perimeter;
+class oneNine {
+	public static void main(String[] args) {
+			
+			float width = 4.5f;
+			float height = 7.9f;
+			float area = width * height;
+			float perimeter = (width + height) * 2;
+						
+			System.out.println("For a Width of: " + width + ", and a Height of: " + height);
+			System.out.println("Area: " + area);
+			System.out.println("Perimeter: " + perimeter);
+	}
 }
-*/
