f1a93832cee1769878f3443ee4619b6305b41ef2
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-17 16:18:07 -0600

    Update 2-23.java

--- Chapter_2/2-23.java
+++ Chapter_2/2-23.java
@@ -1,24 +1,28 @@
+
 /*
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
 Assignment: 2-23
-Date: .  2018
+Date: Sept. 17, 2018
 Summary: (Cost of driving) Write a program that prompts the user to enter 
-          the distance to drive, the fuel efficiency of the car in miles 
-          per gallon, and the price per gallon, and displays the cost of the trip.
+					the distance to drive, the fuel efficiency of the car in miles 
+					per gallon, and the price per gallon, and displays the cost of the trip.
 */
 
-/*
-Main{
-          float distance = user input "Distance (mi):"
-          float efficiency = user input "Efficiency (mi/gal):"
-          float price = user input "Price ($/gal): $"
-          float tripCost;
-          
-          tripCost = distance * efficiency * price;
-          
-          system.out.println("Trip Cost: $" tripcost);
-          
+import java.util.Scanner;
+
+class twoTwentythree {
+	public static void main(String[] args) {
+					Scanner reader = new Scanner(System.in);
+					
+					float distance = user input "Distance (mi):"
+					float efficiency = user input "Efficiency (mi/gal):"
+					float price = user input "Price ($/gal): $"
+					float tripCost;
+					
+					tripCost = distance * efficiency * price;
+					
+					system.out.println("Trip Cost: $" tripcost);				
+	}
 }
-*/
