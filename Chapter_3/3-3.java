805d6122bfdbb908e38f14be4b21a04ebc7b42c1
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-17 18:30:38 -0600

    Update 3-3.java

--- Chapter_3/3-3.java
+++ Chapter_3/3-3.java
@@ -5,19 +5,7 @@
 		Course: CSCI 1105 - Introduction to Programming
 		Assignment: 3-3
 		Date: Sept. 17  2018
-		Summary: (Algebra: solve 2 * 2 linear equations) A linear equation can 
-							be solved using Cramer’s rule given in Programming Exercise 1.13
-							(shown below). Write a program that prompts the user to enter 
-							a, b, c, d, e, and f and displays the result. If ad - bc is 0, 
-							report that “The equation has no solution.”
-									
-									ax + by = e
-									cx + dy = f
-									x = (ed - bf) / (ad - bc)
-									y = (af - ec) / (ad - bc)
-
-							(Hint: Only solve the equations below if ad - bc is not 0.)
-*/
+		Summary:
 
 import java.util.Scanner;
 
