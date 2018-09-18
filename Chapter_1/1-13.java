ea97f01cbbb65273c856b52d8bf192b9733d76e6
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-17 18:31:04 -0600

    Update 1-13.java

--- Chapter_1/1-13.java
+++ Chapter_1/1-13.java
@@ -9,7 +9,6 @@
 							be solved using Cramer’s rule given in Programming Exercise 1.13
 							(shown below). Write a program that prompts the user to enter 
 							a, b, c, d, e, and f and displays the result. If ad - bc is 0, 
-							report that “The equation has no solution.”
 									
 									ax + by = e
 									cx + dy = f
