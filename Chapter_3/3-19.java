9f307ed9b13b174f7186e0083d77ef360d2bc965
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-17 18:00:47 -0600

    Update 3-19.java

--- Chapter_3/3-19.java
+++ Chapter_3/3-19.java
@@ -1,39 +1,41 @@
+
+
 /*
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
 Assignment: 3-19
-Date: .  2018
+Date: Sept. 17, 2018
 Summary: (Compute the perimeter of a triangle) Write a program 
-          that reads three edges for a triangle and computes the 
-          perimeter if the input is valid. Otherwise, display that 
-          the input is invalid. The input is valid if the sum of 
-          every pair of two edges is greater than the remaining edge.
+					that reads three edges for a triangle and computes the 
+					perimeter if the input is valid. Otherwise, display that 
+					the input is invalid. The input is valid if the sum of 
+					every pair of two edges is greater than the remaining edge.
 */
 
-/*
-Main{
-          float sideA = user input "Side A:";
-          float sideB = user input "Side B:";
-          float sideC = user input "Side C:";
-          float perimeter;
-          
-          if (Double.parseDouble(data[user input]) < 0) {
-                    out.println("Must be positive.");
-                    }
-      
-          elif (user input = NaN){
-                    out.println("Must be a number.");
-                    }
-          
-          elif (sideA + side B > sideC){
-                    out.println("Side C must be longer than Side A + Side B.");
-                    }
-          
-          
-          else{
-           perimeter = sideA + sideB + sideC
-           out.println(perimeter);
-           }
+import java.util.Scanner;
+
+class threeNineteen {
+	public static void main(String[] args) {
+					Scanner reader = new Scanner(System.in);
+					
+					System.out.print("Input Side A: ");
+					double sideA = reader.nextDouble();
+					
+					System.out.print("Input Side B: ");
+					double sideB = reader.nextDouble();
+					
+					System.out.print("Input Side C: ");
+					double sideC = reader.nextDouble();
+					
+					double perimeter;
+			
+					if(sideA + sideB > sideC){
+						System.out.println("Side C must be longer than Side A + Side B.");
+										}
+					else{
+					 perimeter = sideA + sideB + sideC;
+					 System.out.println("The Perimeter is: " + perimeter);
+					 }
+	}
 }
-*/
