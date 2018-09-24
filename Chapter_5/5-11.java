a7de988def692633c2a1fcf681446859f2feb970
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-24 15:40:32 -0600

    Update 5-11.java

--- Chapter_5/5-11.java
+++ Chapter_5/5-11.java
@@ -14,41 +14,28 @@ Summary: (Find numbers divisible by 5 or 6, but not both)
 public class fiveEleven{
 	public static void main(String[] args) {
 	
-int x = 100;
-float w = -1;
-float y = -1;
-float z = -1;
-float zz = -1;
-//Object[] numberArray = {,,,,,,,,,}; //arr.length
-System.out.println("x = " + x);
-	while(x <= 200){
-		z = x % 5;
-		zz = x % 6;
+		int x = 100;
+		int y = 2;
 		
-		if(z <= 0 && zz <= 0){
-		w = x; 
-		System.out.println("%5$%6: " + y);	
-		} 
-	
-	else if(z <= 0) {
-		y = x;
-		System.out.println("%5: " + y);
-		//push to array	
+		while(x <= 200 && x >= 100 ){	
+			if(x % 5 == 0 && x % 6 == 0){
+				x++;
+			}
+			else if(x % 5 == 0 || x % 6 == 0){		
+				System.out.print(x + " ");
+				x++;
+				
+				if(y <= 10){
+					y++;
+				}
+				else{
+					System.out.println();
+					y = 2;
+				}
+			}
+			else{
+				x++;
+			}
 		}
-	
-	else if(zz <= 0) {
-		y = x;
-		System.out.println("%6: " + y);
-		//push to array	
-		}
-	
-	else{
-		System.out.println("Not: " + x);
-		}	
-	
-			x++;
-		}
-	//print array
-
 	}
 }
