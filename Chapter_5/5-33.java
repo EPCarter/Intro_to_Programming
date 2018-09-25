7671ece1b65b74733199f00c6b06d2e2b79a6393
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-25 17:20:02 -0600

    Update 5-33.java

--- Chapter_5/5-33.java
+++ Chapter_5/5-33.java
@@ -3,7 +3,7 @@ Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
 Assignment: 5-33
-Date: .  2018
+Date: Sept. 25, 2018
 Summary: (Perfect number) A positive integer is called 
           a perfect number if it is equal to the sum of 
           all of its positive divisors, excluding itself. 
@@ -13,28 +13,27 @@ Summary: (Perfect number) A positive integer is called
           Write a program to find all these four numbers.
 */
 
-public class Test2 {
+public class FiveThirteeThree {
 	public static void main(String[] args) {
 		int x = 1;
 
-		while (x <= 100000) {
+		while (x <= 10000) {
 			x++;
 			int y = 0;
 			int z = 0;
-
-			while (y < x) {
+			boolean done = false;
+			while (y <= x) {
 				y++;
 
 				if (x % y == 0) {
 					z += y;
-					//System.out.println(x + " is divisible by " + y + " sum " + z + ".");
-					if (z == x && x != 24 && x != 8190 && x != 2016) {
-						System.out.println(x + " is a perfect number.");
-						
-					}
-					if (z == x + 1) {
-						// System.out.println(x + " is prime.");
-					}
+					// System.out.println(x + " is divisible by " + y + " sum " + z + ".");
+				}
+				if (z == x + 1) {
+					// System.out.println(x + " is prime.");
+				}
+				if (z == x && y >= x - 1) { // x != 24 && x != 8190 && x != 2016
+					System.out.println(x + " is a perfect number.");
 				}
 			}
 		}
