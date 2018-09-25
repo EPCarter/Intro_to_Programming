80c2ea69e0c7fd8e64404301e3b48ddf70308627
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-25 02:08:59 -0600

    Update 5-17.java

--- Chapter_5/5-17.java
+++ Chapter_5/5-17.java
@@ -1,44 +1,40 @@
-import java.util.*;	
 
-class Untitled {
+import java.util.*;
+
+public class Test {
 	public static void main(String[] args) {
-	
 		System.out.print("Enter the number of lines: ");
-		Scanner reader = new Scanner(System.in);  
+		Scanner reader = new Scanner(System.in);
 		int lines = reader.nextInt();
+		reader.close(); // 10, 12, 13 break
 		int printedRow = 1;
-		
-		while(printedRow <= lines && printedRow <= 15 && printedRow >= 1){
-			
+		while (printedRow <= lines && printedRow <= 150 && printedRow >= 1) {
 			int spaces = 1;
-			
-			while(spaces <= lines - printedRow){
-				System.out.print(".");
+			String interNumberSpace = " ";
+			String extraNumberSpace = " ";
+			while (spaces <= lines - printedRow) {
+				System.out.print(extraNumberSpace);
+				System.out.print(interNumberSpace);
+				if (printedRow > 9) {
+					extraNumberSpace = "  ";
+				}
 				spaces++;
 			}
-	
-	int count = printedRow;
-	while(count >= 1 && count <= printedRow ){
-		if(count > 1){
-			System.out.print(count);
-			count--;
-		}
-	}	
-
-		
-		//System.out.print(spaces);
-		//System.out.print(" " + printedRow);
-		//System.out.print(" " + lines);
-			
-		
-			
-		while(spaces > 1){
-			System.out.print(".");
-			spaces--;
-		}			
+			int count = printedRow;
+			while (count > 1) {
+				System.out.print(count + interNumberSpace);
+				count--;
+			}
+			while (count <= printedRow) {
+				System.out.print(count + interNumberSpace);
+				count++;
+			}
+			while (spaces > 1) {
+				System.out.print(interNumberSpace);
+				spaces--;
+			}
 			printedRow++;
 			System.out.println();
 		}
-		
 	}
 }
