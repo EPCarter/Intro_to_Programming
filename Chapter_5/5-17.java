64ea0d16f2a57fbb4ff088b26c06e2fba5d992a7
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-24 19:55:56 -0600

    Update 5-17.java

--- Chapter_5/5-17.java
+++ Chapter_5/5-17.java
@@ -1,33 +1,44 @@
-/*
-Author: Erik Carter
-Email: Carter.Eri7200@stu.stech.edu
-Course: CSCI 1105 - Introduction to Programming
-Assignment: 5-17
-Date: .  2018
-Summary: (Display pyramid) Write a program that prompts
-          the user to enter an integer from 1 to 15 and 
-          displays a pyramid, as shown in the following 
-          sample run.
-          
-          Enter Number of Lines: 3 (user input)
-          
-           1
-          212
-         32123
-*/
-import java.util.Scanner;
+import java.util.*;	
 
-class fiveSeventeen {
-         public static void main(String[] args) {
-                    Scanner reader = new Scanner(System.in);  
-                    int lines = reader.nextInt();
-                   
-                   Array1[]
-                   Array2[]
-                   Array3[]
-                       ...15
-                             
-                  System.out.println("Lines Given: " + lines);
-                  System.out.println(array);
-          }
+class Untitled {
+	public static void main(String[] args) {
+	
+		System.out.print("Enter the number of lines: ");
+		Scanner reader = new Scanner(System.in);  
+		int lines = reader.nextInt();
+		int printedRow = 1;
+		
+		while(printedRow <= lines && printedRow <= 15 && printedRow >= 1){
+			
+			int spaces = 1;
+			
+			while(spaces <= lines - printedRow){
+				System.out.print(".");
+				spaces++;
+			}
+	
+	int count = printedRow;
+	while(count >= 1 && count <= printedRow ){
+		if(count > 1){
+			System.out.print(count);
+			count--;
+		}
+	}	
+
+		
+		//System.out.print(spaces);
+		//System.out.print(" " + printedRow);
+		//System.out.print(" " + lines);
+			
+		
+			
+		while(spaces > 1){
+			System.out.print(".");
+			spaces--;
+		}			
+			printedRow++;
+			System.out.println();
+		}
+		
+	}
 }
