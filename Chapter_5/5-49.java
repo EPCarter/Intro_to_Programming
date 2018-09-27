1700645c594c581f6a36ec554335d7f723b3bc1f
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-09-26 19:14:06 -0600

    Update 5-49.java

--- Chapter_5/5-49.java
+++ Chapter_5/5-49.java
@@ -11,38 +11,45 @@ Summary: (Count vowels and consonants) Assume letters A, E, I, O, and U as the v
 import java.util.regex.*;
 import java.util.*;
 
-public class Space {
-
+public class Space1 {
+	int vowelCount = 0;
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		System.out.print("Enter a string: ");
 		String input = reader.nextLine();
-		System.out.println("The String is:" + input);
+		//System.out.println("The String is:" + input);
 		reader.close();
-
+		
 		String aPattern = "(()?(A+|a+)+()?)+";
 		String ePattern = "(()?(E+|e+)+()?)+";
 		String iPattern = "(()?(I+|i+)+()?)+";
 		String oPattern = "(()?(O+|o+)+()?)+";
 		String uPattern = "(()?(U+|u+)+()?)+";
 		String yPattern = "(()?(Y+|y+)+()?)+";
+		String vowelPattern = "(()?(a+|e+|i+|o+|u+|y+|A+|E+|I+|O+|U+|Y+)+()?)+";
+		String nonvowelPattern = "[^aeiouyAEIOUY]+";
 
-		regexChecker(yPattern, input);
 		regexChecker(aPattern, input);
 		regexChecker(ePattern, input);
 		regexChecker(iPattern, input);
 		regexChecker(oPattern, input);
 		regexChecker(uPattern, input);
+		regexChecker(yPattern, input);
+		System.out.println(" are vowels.");
 
-		System.out.print(" are vowels.");
+		System.out.print("Vowels: ");
+		regexChecker(vowelPattern, input);
+		
+		System.out.println();
+		System.out.print("Consonants: ");
+		regexChecker(nonvowelPattern, input);
 	}
 
 	public static void regexChecker(String theRegex, String str2Check) {
 		Pattern checkRegex = Pattern.compile(theRegex);
 		Matcher regexMatcher = checkRegex.matcher(str2Check);
 		if (regexMatcher.find()) {
-			System.out.println(" " + regexMatcher.group().trim());
+			System.out.print(" " + regexMatcher.group().trim());
 		}
-
 	}
 }
