ddab25c0652847a9240b6bb8e7e1cebe858fb214
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-10-01 18:24:33 -0600

    Update 7-5.java

--- Chapter_7/7-5.java
+++ Chapter_7/7-5.java
@@ -15,10 +15,35 @@ Summary: (Print distinct numbers) Write a program that reads in ten
           After the input, the array contains the distinct numbers.
 */
 
-import java.util.Scanner;
+import java.util.*;
 
-class sevenFive {
-          public static void main(String[] args) {
-           Scanner reader = new Scanner(System.in);         
-          }
+class SevenFive {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		int[] numberArray = new int[11];
+		int[] numberArrayPrint = new int[numberArray.length];
+		int number = 1;
+		while (number <= 10) {
+			System.out.print("Enter number " + number + ": ");
+			int input = reader.nextInt();
+			numberArray[number] = input;
+			number++;
+		}
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if (numberArray[i] == numberArray[j] && i != j) {
+					numberArrayPrint[j] = 0;
+				} 
+				else {
+					numberArrayPrint[i] = numberArray[i];
+				}
+			}
+		}
+		for (int i = 1; i < numberArrayPrint.length; i++) {
+			if (numberArrayPrint[i] == 0) {
+			} else {
+				System.out.print(numberArrayPrint[i] + " ");
+			}
+		}
+	}
 }
