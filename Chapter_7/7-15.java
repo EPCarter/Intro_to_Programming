29fbccfa4c5d69ad2e1da0f6e350ced8862c2b56
EPCarter <39419198+EPCarter@users.noreply.github.com>
2018-10-01 18:20:47 -0600

    Update 7-15.java

--- Chapter_7/7-15.java
+++ Chapter_7/7-15.java
@@ -20,10 +20,35 @@ Summary: (Eliminate duplicates) Write a method that returns a
           the user's array.)
 */
 
-import java.util.Scanner;
+import java.util.*;
 
-class sevenFifteen {
-          public static void main(String[] args) {
-           Scanner reader = new Scanner(System.in);         
-          }
+class Spaces3 {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		int[] numberArray = new int[11];
+		int[] numberArrayPrint = new int[numberArray.length];
+		int number = 1;
+		while (number <= 10) {
+			System.out.print("Enter number " + number + ": ");
+			int input = reader.nextInt();
+			numberArray[number] = input;
+			number++;
+		}
+		reader.close();
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if (numberArray[i] == numberArray[j] && i != j) {
+					numberArrayPrint[j] = 0;
+				} else {
+					numberArrayPrint[i] = numberArray[i];
+				}
+			}
+		}
+		for (int i = 1; i < numberArrayPrint.length; i++) {
+			if (numberArrayPrint[i] == 0) {
+			} else {
+				System.out.print(numberArrayPrint[i] + " ");
+			}
+		}
+	}
 }
