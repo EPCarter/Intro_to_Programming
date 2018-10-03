b8b4c5af5e60973f85eead0226631d55e88c21e7
EPCarter <Carter.Eri7200@stu.stech.edu>
2018-10-02 19:34:19 -0600

    EightFive Finished again

--- Chapter_8/EightFive.java
+++ Chapter_8/EightFive.java
@@ -1,4 +1,5 @@
 package Chapter_8;
+
 /*
 Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
@@ -20,61 +21,74 @@ Summary: (Algebra: add two matrices) Write a method to add two matrices.
                   4.0   5.0   6.0 + 1.0  4.5  2.2 = 5.0  9.5  8.2
                   7.0   8.0   9.0   1.1  4.3  5.2   8.1  12.3  14.2
 */
-
 import java.util.Scanner;
 
 class eightFive {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		double[] matrix0 = new double[19];
-		double[][] matrix1 = new double[3][3];
-		double[][] matrix2 = new double[3][3];
-		double[][] matrix3 = new double[3][3];
+		int k = -1;
+		int l = -1;
+		int m = 0;
+		int n = 0;
 
-		for (int i = 1; i <= 18; i++) {
-			System.out.print("Enter number " + i + ": ");
-			String input = reader.nextLine();
-			matrix0[i] = Double.parseDouble(input);
+		System.out.println("Matrices must be the same size.");
+		do {
+			System.out.print("First box width: ");
+			k = reader.nextInt();
+			System.out.print("First box height: ");
+			l = reader.nextInt();
+			System.out.print("Second box height: ");
+			m = reader.nextInt();
+			System.out.print("Second box height: ");
+			n = reader.nextInt();
+		} while (k != m || l != n);
+
+		double[] matrix0 = new double[(k * l) * 2];
+		double[][] matrix1 = new double[k][l];
+		double[][] matrix2 = new double[m][n];
+		double[][] matrix3 = new double[k][l];
+		int o = (k * l) * 2;
+
+		for (int i = 0; i < o; i++) {
+			System.out.print("Enter number " + (i + 1) + ": ");
+			double input = reader.nextDouble();
+			matrix0[i] = input;
 		}
 		reader.close();
+		int x = 0;
+		for (int p = 0; p < k; p++) {
+			for (int q = 0; q < l; q++) {
+				matrix1[p][q] = matrix0[x];
+				x++;
+			}
+		}
 
-		matrix1[0][0] = matrix0[1];
-		matrix1[0][1] = matrix0[2];
-		matrix1[0][2] = matrix0[3];
-		matrix1[1][0] = matrix0[4];
-		matrix1[1][1] = matrix0[5];
-		matrix1[1][2] = matrix0[6];
-		matrix1[2][0] = matrix0[7];
-		matrix1[2][1] = matrix0[8];
-		matrix1[0][2] = matrix0[9];
-
-		matrix2[0][0] = matrix0[10];
-		matrix2[0][1] = matrix0[11];
-		matrix2[0][2] = matrix0[12];
-		matrix2[1][0] = matrix0[13];
-		matrix2[1][1] = matrix0[14];
-		matrix2[1][2] = matrix0[15];
-		matrix2[2][0] = matrix0[16];
-		matrix2[2][1] = matrix0[17];
-		matrix2[0][2] = matrix0[18];
+		for (int r = 0; r < m; r++) {
+			for (int s = 0; s < n; s++) {
+				matrix2[r][s] = matrix0[x];
+				x++;
+			}
+		}
 
 		matrix3 = addMatrix(matrix1, matrix2);
-		System.out.println(matrix3[0][0] + " " + matrix3[0][1] + " " + matrix3[0][2]);
-		System.out.println(matrix3[1][0] + " " + matrix3[1][1] + " " + matrix3[1][2]);
-		System.out.println(matrix3[2][0] + " " + matrix3[2][1] + " " + matrix3[2][2]);
+
+		for (int w = 0; w < matrix3.length; w++) {
+			for (int c = 0; c < matrix3[0].length; c++) {
+				System.out.print(matrix3[w][c] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	public static double[][] addMatrix(double[][] a, double[][] b) {
-		double[][] c = new double[3][3];
-		c[0][0] = a[0][0] + b[0][0];
-		c[0][1] = a[0][1] + b[0][1];
-		c[0][2] = a[0][2] + b[0][2];
-		c[1][0] = a[1][0] + b[1][0];
-		c[1][1] = a[1][1] + b[1][1];
-		c[1][2] = a[1][2] + b[1][2];
-		c[2][0] = a[2][0] + b[2][0];
-		c[2][1] = a[2][1] + b[2][1];
-		c[2][2] = a[2][2] + b[2][2];
+		int z = a.length;
+		int y = a[0].length;
+		double[][] c = new double[z][y];
+		for (int w = 0; w < z; w++) {
+			for (int x = 0; x < y; x++) {
+				c[w][x] = a[w][x] + b[w][x];
+			}
+		}
 		return c;
 	}
 }
