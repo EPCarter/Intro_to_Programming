a42083fd67ccc1933c90e82d657f189fbdac4ba5
EPCarter <Carter.Eri7200@stu.stech.edu>
2018-10-15 19:50:30 -0600

    RamJack continued

--- Final_Project/FinalProject.java
+++ Final_Project/FinalProject.java
@@ -4,22 +4,24 @@ Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
 Assignment: Final Project
-Date: Oct. 10, 2018
+Date: Oct. 15, 2018
 Summary: Display info about points near a given address.
 */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("resource")
 public class FinalProject {
 	public static void main(String[] args) {
 		String input = inputer();
 		List<List<String>> cleanedData = importer(input);
-		printer(cleanedData);
+		finder(cleanedData);
 	}
 
-	public static String inputer() {// Takes address input
+	public static String inputer() {// Takes an address input
 		System.out.println("Address: ");
 		Scanner reader = new Scanner(System.in);
 		String input = reader.nextLine();
@@ -27,75 +29,53 @@ public class FinalProject {
 	}
 
 	public static List<List<String>> importer(String input) { // imports csv file to raw array list
-		String fileName = "/Users/Administrator/Git/Intro_to_Programming/Final_Project/Database.csv";
-		File file = new File(fileName);
 		List<List<String>> rawData = new ArrayList<>();
-		List<List<String>> cleanedData = new ArrayList<>();
-
 		if (input != "") {
 			String line = input;
-			String[] values = line.split("   "); // .split(",");
+			String[] values = line.split(",");
 			rawData.add(Arrays.asList(values));
 		}
-
+		String fileName = "/Users/Administrator/Git/Intro_to_Programming/Final_Project/Database.csv";
+		File file = new File(fileName);
 		Scanner inputStream;
 		try {
 			inputStream = new Scanner(file);
 			while (inputStream.hasNext()) {
 				String line = inputStream.nextLine();
-				String[] values = line.split("   "); // .split(",");
+				String cleanPattern = "-"; // remove dashes
+				line = line.replaceAll(cleanPattern, " ");
+				String[] values = line.split(",");
 				rawData.add(Arrays.asList(values));
 			}
 			inputStream.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		cleanedData = listCleaner(rawData);
-		return cleanedData;
-	}
-
-	public static List<List<String>> listCleaner(List<List<String>> rawData) {
 		List<List<String>> cleanedData = new ArrayList<>();
-		int lineNo = 1;
-		for (List<String> line : rawData) {
-			int columnNo = 1;
-			for (String value : line) {
-				String line2 = value;
-				line2 = stringCleaner(line2, rawData);
-				String[] values = line2.split("   "); // .split(",");
-				cleanedData.add(Arrays.asList(values));
-				columnNo++;
-			}
-			lineNo++;
-		}
+		cleanedData = rawData; // through cleaner method
 		return cleanedData;
 	}
 
-	public static String stringCleaner(String input, List<List<String>> rawData) { // finds data in given string
-		String cleanedString = input;
-		// compare to array spot
-		// for (int i = 0; i < rawData.size(); i++) {
-		// if(rawData[i].matches(input)) { //if input matches array spot i
-		System.out.println("add to array at spot i");
+	public static void finder(List<List<String>> cleanedData) {
+		// 0 = input
+		// 1 = title
+		// 2 = regex
+		// 3 = -blank-
+		// 4 = database entry 1
 
-		// }
-		// cleanedString = cleanedString.replaceAll(cleanPattern, "");
-		return cleanedString;
-
-	}
-
-	public static void puller() { // gets info from google maps api
-	}
-
-	public static void printer(List<List<String>> cleanedData) { // prints array
-		int lineNo = 1;
-		for (List<String> line : cleanedData) {
-			int columnNo = 1;
-			for (String value : line) {
-				System.out.println("[" + lineNo + "]" + "[" + columnNo + "]-" + value);
-				columnNo++;
+		for (int i = 4; i < cleanedData.size(); i++) {
+			String line = cleanedData.get(i).toString();
+			for (int j = 0; j < 18; j++) {
+				String input = cleanedData.get(0).toString();
+				try {
+					String regex = cleanedData.get(2).get(j).toString();
+					//System.out.println(regex);
+					if (input.matches(regex)) {
+						System.out.println(line);
+					}
+				} catch (IndexOutOfBoundsException e) {
+				}
 			}
-			lineNo++;
 		}
 	}
 }
--- Final_Project/Scratch.java
+++ Final_Project/Scratch.java
@@ -21,28 +21,46 @@ public class Scratch {
 		finder(rawData, cleanedData); // find criteria for new list
 		filler(cleanedData); // google search for missing
 		comparer(cleanedData); // compare input to others
-		printer(cleanedData); // print results
+		printer(rawData); // print results
 	}
 
 	public static List<List<String>> inputer(List<List<String>> rawData) {
 		System.out.println("Address: ");
-		System.out.println("Address: ");
 		Scanner reader = new Scanner(System.in);
 		String input = reader.nextLine();
+		if (input != "") {
+			String line = input;
+			String[] values = line.split("   "); // .split(",");
+			rawData.add(Arrays.asList(values));
+		}
 		return rawData;
-		// scanner input
-		// add to rawData
 	}
 
 	public static List<List<String>> importer(List<List<String>> rawData) {
-		// import cvs file
-		// add to rawData
+		String fileName = "/Users/Administrator/Git/Intro_to_Programming/Final_Project/Database.csv";
+		File file = new File(fileName);
+		Scanner inputStream;
+		try {
+			inputStream = new Scanner(file);
+			while (inputStream.hasNext()) {
+				String line = inputStream.nextLine();
+				String[] values = line.split("   "); // .split(",");
+				rawData.add(Arrays.asList(values));
+			}
+			inputStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		return rawData;
 	}
 
 	public static List<List<String>> finder(List<List<String>> rawData, List<List<String>> cleanedData) {
 		// find regex in row 0 in all other slots
 		// if found, add to pertinent cleanedList slot
+		// for (int i = 0; i < cleanedData.size(); i++) {
+		// System.out.println(cleanedData.replaceAll(i));
+		// }
+		// cleanedData = cleanedData[i].replaceAll(",,", "");
 		return cleanedData;
 	}
 
@@ -60,6 +78,61 @@ public class Scratch {
 
 	public static void printer(List<List<String>> cleanedData) {
 		// print top ten distance list of type
-		System.out.println(".");
+		for (int i = 0; i < 8; i++) { // cleanedData.size()
+			System.out.println(cleanedData.get(i));
+		}
 	}
+
+	public static List<List<String>> listCleaner(List<List<String>> rawData) {
+		List<List<String>> cleanedData = new ArrayList<>();
+		int lineNo = 1;
+		for (List<String> line : rawData) {
+			int columnNo = 1;
+			for (String value : line) {
+				String line2 = value;
+				// line2 = stringCleaner(line2, rawData);
+				finder(cleanedData);
+				String[] values = line2.split(",");
+				cleanedData.add(Arrays.asList(values));
+				columnNo++;
+			}
+			lineNo++;
+		}
+		return cleanedData;
+	}
+
+	public static String stringCleaner(String input, List<List<String>> rawData) { // removes garbage from input and
+																					// database if needed
+		String cleanedString = input;
+		String cleanPattern = ",";
+		cleanedString = cleanedString.replaceAll(cleanPattern, "");
+		return cleanedString;
+	}
+
+	public static void puller() { // gets info from google maps api
+	}
+
+	public static void printer(List<List<String>> cleanedData) { // prints info
+		int lineNo = 1;
+		for (List<String> line : cleanedData) {
+			int columnNo = 1;
+			for (String value : line) {
+				System.out.println("[" + lineNo + "]" + "[" + columnNo + "] " + value);
+				columnNo++;
+			}
+			lineNo++;
+		}
+		/*
+		 * System.out.println(cleanedData.get(4));
+		 * 
+		 */
+
+	}
+	// ListIterator<List<String>> liter = cleanedData.listIterator();
+			Iterator<List<String>> iter = cleanedData.iterator();
+			while (iter.hasNext()) {
+				Iterator<String> siter = iter.next().iterator();
+				while (siter.hasNext()) {
+					String s = siter.next();
+					// System.out.println(s);
 }
