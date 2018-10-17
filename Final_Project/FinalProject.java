7c1187efa6188dc26dbb6f5bef7c304ed3b0b66a
EPCarter <Carter.Eri7200@stu.stech.edu>
2018-10-16 22:07:18 -0600

    RamJack working

--- Final_Project/FinalProject.java
+++ Final_Project/FinalProject.java
@@ -4,14 +4,12 @@ Author: Erik Carter
 Email: Carter.Eri7200@stu.stech.edu
 Course: CSCI 1105 - Introduction to Programming
 Assignment: Final Project
-Date: Oct. 15, 2018
+Date: Oct. 16, 2018
 Summary: Display info about points near a given address.
 */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @SuppressWarnings("resource")
 public class FinalProject {
@@ -19,10 +17,11 @@ public class FinalProject {
 		String input = inputer();
 		List<List<String>> cleanedData = importer(input);
 		finder(cleanedData);
+		printer(cleanedData);
 	}
 
 	public static String inputer() {// Takes an address input
-		System.out.println("Address: ");
+		System.out.println("Lookup (comma for more criteria): ");
 		Scanner reader = new Scanner(System.in);
 		String input = reader.nextLine();
 		return input;
@@ -31,8 +30,8 @@ public class FinalProject {
 	public static List<List<String>> importer(String input) { // imports csv file to raw array list
 		List<List<String>> rawData = new ArrayList<>();
 		if (input != "") {
-			String line = input;
-			String[] values = line.split(",");
+			String inputRow = input;
+			String[] values = inputRow.split(",");
 			rawData.add(Arrays.asList(values));
 		}
 		String fileName = "/Users/Administrator/Git/Intro_to_Programming/Final_Project/Database.csv";
@@ -41,9 +40,7 @@ public class FinalProject {
 		try {
 			inputStream = new Scanner(file);
 			while (inputStream.hasNext()) {
-				String line = inputStream.nextLine();
-				String cleanPattern = "-"; // remove dashes
-				line = line.replaceAll(cleanPattern, " ");
+				String line = cleaner(inputStream.nextLine());
 				String[] values = line.split(",");
 				rawData.add(Arrays.asList(values));
 			}
@@ -52,30 +49,81 @@ public class FinalProject {
 			e.printStackTrace();
 		}
 		List<List<String>> cleanedData = new ArrayList<>();
-		cleanedData = rawData; // through cleaner method
+		cleanedData = rawData;
 		return cleanedData;
 	}
 
-	public static void finder(List<List<String>> cleanedData) {
-		// 0 = input
-		// 1 = title
-		// 2 = regex
-		// 3 = -blank-
-		// 4 = database entry 1
-
-		for (int i = 4; i < cleanedData.size(); i++) {
-			String line = cleanedData.get(i).toString();
-			for (int j = 0; j < 18; j++) {
-				String input = cleanedData.get(0).toString();
-				try {
-					String regex = cleanedData.get(2).get(j).toString();
-					//System.out.println(regex);
-					if (input.matches(regex)) {
-						System.out.println(line);
-					}
-				} catch (IndexOutOfBoundsException e) {
+	public static List<List<String>> finder(List<List<String>> cleanedData) {
+		List<List<String>> found = new ArrayList<>();
+		for (int j = 0; j < cleanedData.get(0).size(); j++) {
+			for (int i = 1; i < cleanedData.size(); i++) {
+				String inputRow = cleanedData.get(0).get(j).toString();
+				String row = cleanedData.get(i).toString();
+				boolean contains = row.toUpperCase().contains(inputRow.toUpperCase());
+				if (contains) {
+					found.add(Arrays.asList(row));
 				}
 			}
 		}
+		String founded = found.toString();
+		String[] founds = founded.split(",");
+		cleanedData.add(Arrays.asList(founds));
+		return cleanedData; // including found rows in last row
+	}
+
+	public static String regex(int k) {
+		String regex = "";
+		String address = "()"; // number word word
+		String city = "()"; // word
+		String state = "(UT|NV)"; // always ut or nv
+		String zip = "()"; // always 5 digit (maybe +4)
+		String lon = "()"; // always pos 2 digit
+		String lat = "()"; // always neg 3 digit
+		String type = "()"; // word
+
+		switch (k) {
+		case 1:
+			regex = address;
+			break;
+		case 2:
+			regex = city;
+			break;
+		case 3:
+			regex = state;
+			break;
+		case 4:
+			regex = zip;
+			break;
+		case 5:
+			regex = lon;
+			break;
+		case 6:
+			regex = lat;
+			break;
+		case 7:
+			regex = type;
+			break;
+		default:
+			regex = "";
+			break;
+		}
+		return regex;
+	}
+
+	public static void printer(List<List<String>> cleanedData) {
+		for (int i = 0; i < cleanedData.get(cleanedData.size() - 1).get(0).length(); i++) {
+			try {
+				String output = cleaner(cleanedData.get(cleanedData.size() - 1).get(i));
+				System.out.println(output);
+			} catch (IndexOutOfBoundsException e) { // figure out what's up
+			}
+		}
+	}
+
+	public static String cleaner(String toClean) {
+		String cleanedString = toClean.replaceAll("\\,|\\*|\\(|\\)|\\[|\\]|\\:|\\'|\\^|\\?|\"|\\.", " ");
+		cleanedString = cleanedString.replaceAll("\\s{2,}", " ");
+		cleanedString = cleanedString.replaceAll("\\s{2,}", " ");
+		return cleanedString;
 	}
 }
--- Final_Project/Scratch.java
+++ Final_Project/Scratch.java
@@ -128,11 +128,41 @@ public class Scratch {
 		 */
 
 	}
+
 	// ListIterator<List<String>> liter = cleanedData.listIterator();
-			Iterator<List<String>> iter = cleanedData.iterator();
-			while (iter.hasNext()) {
+	Iterator<List<String>> iter = cleanedData.iterator();while(iter.hasNext())
+	{
 				Iterator<String> siter = iter.next().iterator();
 				while (siter.hasNext()) {
 					String s = siter.next();
 					// System.out.println(s);
-}
+
+					String cleanPattern = ""; // remove dashes //extra spaces
+					line = line.replaceAll(cleanPattern, " ");
+					try {
+					} catch (IndexOutOfBoundsException e) {
+					}
+					// String regex = regex(k);
+					// find inputRow in row (return row)
+					// find regex in row or column (return whole row)
+					// add found row to next column spot of the last row of cleanedData
+					// add closest ten to next column spot of the last row of cleanedData
+					// k++; // next regex
+					// for (int j = 0; j < cleanedData.get(i).size(); j++) {
+					// String column = cleanedData.get(i).get(j).toString();
+					// }
+				}
+				//if (isMatched) {
+				//	System.out.println("Matched" + row);
+				//}
+				//boolean isMatched = row.regionMatches(true, 0, inputRow, 0, inputRow.length());
+				// inputRow = inputRow.replaceAll(",|\\*|\\[|\\]|\\:|\\'|\\^|\\?", " ");
+				for (int j = 0; j < inputList.size(); j++) {
+					inputRow = inputList.get(j).toString();
+					
+				
+				}
+				List<List<String>> inputList = new ArrayList<>();
+				String[] inputValues = inputRow.split(",");
+				inputList.add(Arrays.asList(inputValues));
+				inputRow = inputList.toString();
